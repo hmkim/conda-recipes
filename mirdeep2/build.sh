c03e5b306bf118260f73bb9aa7f912a72a13908a
hyunmin.kim <brandon@soybean.ptbio.kr>
2016-08-18 16:12:19 +0900

    update

--- mirdeep2/build.sh
+++ mirdeep2/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-target=$PREFIX/lib/mirdeep2
-mkdir -p $target
+#target=$PREFIX/lib/mirdeep2
+#mkdir -p $target
 mkdir -p $PREFIX/bin
-cp src/*pl $target
-ln -s $target/miRDeep2.pl $PREFIX/bin/miRDeep2.pl
-ln -s $target/mapper.pl $PREFIX/bin/mapper.pl
-chmod 775 $PREFIX/bin/miRDeep2.pl
-chmod 775 $PREFIX/bin/mapper.pl
+#cp src/*pl $target
+chmod 775 src/*.pl
+cp -r src/*.pl $PREFIX/bin
+#ln -s $target/miRDeep2.pl $PREFIX/bin/miRDeep2.pl
+#ln -s $target/mapper.pl $PREFIX/bin/mapper.pl
+#chmod 775 $PREFIX/bin/miRDeep2.pl
+#chmod 775 $PREFIX/bin/mapper.pl
 touch $PREFIX/bin/../install_successful
